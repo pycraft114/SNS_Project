d685b8a5da991a5542bfc7b455ca4a2495752a57
Chunho Park <pride3260@gmail.com>
2017-03-31 13:44:44 +0900

    login 기능 (#23)
    * Update README.md
    
    * Update README.md
    
    * changes class names
    
    * Install modules
    
    passport, passport-local, express-session, connect-flash
    
    * Add file join.html, Mod file main.css
    
    * Mod file join.html
    
    * Create folder and files
    
    router/index.js
    router/join.js
    router/login.js
    views/login.ejs
    views/join.ejs
    views/main.ejs
    
    and mod main.css
    
    * Mod app.js
    
    * Mod README.md
    
    * Mod and Create files
    
    mod : views/main.ejs, router/index.js
    create : router/logout.js
    
    * Update router/join.js, views/join.ejs
    
    form action -> ajax
    
    * Delete file & app.js <- app2.js
    
    * Mod .DS_Store

--- app2.js
+++ app2.js
@@ -1,28 +1,20 @@
-
 var express = require('express')
 var app = express()
 var bodyParser = require('body-parser')
-var mysql = require('mysql')
 var router = require('./router/index')
 var passport = require('passport')
 var LocalStrategy = require('passport-local').Strategy
 var session = require('express-session')
 var flash = require('connect-flash')
 
-var connection = mysql.createConnection({ // mysql에 사용에 관한 사용자 정보 객체 생성
-  host : '192.168.56.101',
-  port : '3306',
-  user : 'test',
-  password : 'pw1234',
-  database : 'snsdb'
-})
 
-connection.connect()
+app.listen(3000, function(){
+	console.log("start! express server on port 3000");
+});
 
 app.use(express.static('public'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
-
 app.set('view engine', 'ejs')
 
 app.use(session({
@@ -36,9 +28,6 @@ app.use(flash())
 
 app.use(router)
 
-app.listen('3400', function() {
-  console.log('Server Start Port 3400!')
-})
 
 app.get('/main', function(req, res) {
 	console.log(req.session.passport.user)
